16ea647e7b7ca9329d7b452085cf34e5d30b7d62
himanshu12345yadav <2019kuec2009@iiitkota.ac.in>
2020-07-09 00:05:24 -0500

    Updates

---
+++ precache-manifest.ea2351cb25ab07ab66a54ec63dd806f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25dabf7d77658ac8855d5c32721cae46",
+    "url": "/basic-portfolio-site/index.html"
+  },
+  {
+    "revision": "170fbeb306ff2e13e717",
+    "url": "/basic-portfolio-site/static/css/main.c1ae4b09.chunk.css"
+  },
+  {
+    "revision": "8d4705cbc552bba4b366",
+    "url": "/basic-portfolio-site/static/js/2.4b0e0721.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-portfolio-site/static/js/2.4b0e0721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170fbeb306ff2e13e717",
+    "url": "/basic-portfolio-site/static/js/main.7917d232.chunk.js"
+  },
+  {
+    "revision": "6a0b9cd3e622e149201c",
+    "url": "/basic-portfolio-site/static/js/runtime-main.e619bcb4.js"
+  },
+  {
+    "revision": "3a9323cada1f897401bc10fb7799c385",
+    "url": "/basic-portfolio-site/static/media/background.3a9323ca.jpg"
+  },
+  {
+    "revision": "55ed10e1bca1a27f18df02aa5d1b085c",
+    "url": "/basic-portfolio-site/static/media/profile_image.55ed10e1.jpg"
+  }
+]);
