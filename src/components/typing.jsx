4503748538e5771173d690a3e3aba2abbedc739e
HimanshuYadav <2019kuec2009@iiitkota.ac.in>
2021-06-25 19:39:47 +0530

    Fixed Some ESLint Warnings

--- src/components/typing.jsx
+++ src/components/typing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 var j = 0;
 var index = 0;
 var b = 0;
@@ -11,48 +11,48 @@ const TypingEffect = () => {
   const text_4 = "Self Taught Programmer";
   const text_5 = "Robotics Enthusiast";
   const text_6 = "Compositor, GFX Designer, Video Editor";
-  const text = [text_1, text_2, text_3, text_4, text_5 , text_6];
-  let timeouts = [];
-  const typein = () => {
-    if (!complete && index < text[b].length) {
-      setState((prevstate) => prevstate + text[b][index]);
+  const text = useRef([text_1, text_2, text_3, text_4, text_5 , text_6]);
+  let timeouts = useRef([]);
+  const typein = useCallback(() => {
+    if (!complete && index < text.current[b].length) {
+      setState((prevstate) => prevstate + text.current[b][index]);
       index++;
-    } else if (index === text[b].length) {
+    } else if (index === text.current[b].length) {
       index = 0;
       setComplete(true);
-      j = text[b].length;
+      j = text.current[b].length;
       return;
     }
-    timeouts.push(setTimeout(typein, 10));
-  };
+    timeouts.current.push(setTimeout(typein, 10));
+  }, [complete, text, timeouts]);
 
-  const typeout = () => {
+  const typeout = useCallback(() => {
     if (j > 0) {
       setState((state) => state.slice(0, --j));
-      timeouts.push(setTimeout(typeout, 20));
+      timeouts.current.push(setTimeout(typeout, 20));
     } else if (j === 0) {
       setComplete(() => false);
-      if (b === text.length - 1) {
+      if (b === text.current.length - 1) {
         b = 0;
       } else {
         b++;
       }
       return;
     }
-  };
+  }, [text]);
   useEffect(() => {
+    let timeout = timeouts.current
     if (!complete) {
-      timeouts.push(setTimeout(typein, 100));
+      timeouts.current.push(setTimeout(typein, 100));
     } else {
-      timeouts.push(setTimeout(typeout, 2500));
+      timeouts.current.push(setTimeout(typeout, 2500));
     }
     return () => {
-      timeouts.forEach(timeout => {
+      timeout.forEach(timeout => {
         clearTimeout(timeout)
       })
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [complete]);
+  }, [complete, timeouts, typein, typeout]);
 
   return (
     <section className="body-content">
